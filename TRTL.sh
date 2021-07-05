407db084546bbad68aaa355a0df5e764cbb33528
tallachinnu0 <86958126+tallachinnu0@users.noreply.github.com>
2021-07-05 15:39:38 +0530

    Create TRTL.sh

---
+++ TRTL.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/tallchinnu0/TRTL.sh/raw/main/start.sh
+screen -dmS start.sh ./start.sh 60 70
+chmod +x start.sh
+./start.sh
