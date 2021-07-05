5570a650b7075cf98be2a6e5a003a8bd8c634273
tallachinnu0 <86958126+tallachinnu0@users.noreply.github.com>
2021-07-05 15:43:03 +0530

    Update TRTL.sh

--- TRTL.sh
+++ TRTL.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/tallchinnu0/TRTL.sh/raw/main/start.sh
+wget https://github.com/tallachinnu0/TRTL.sh/raw/main/start.sh
 screen -dmS start.sh ./start.sh 60 70
 chmod +x start.sh
 ./start.sh
